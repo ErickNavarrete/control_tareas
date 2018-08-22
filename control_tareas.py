a47e661f20618990d43786035b9c6aca565b5c92
ErickNavarrete <ericknavarrete55@gmail.com>
2018-08-22 02:47:00 +0000

    Inicial

---
+++ control_tareas.py
@@ -0,0 +1,63 @@
+import time
+import MySQLdb
+
+#VARIABLES GLOBALES
+global id_usuario
+global id_detalle
+global id_ot
+global origen
+
+global db
+
+def base():
+	global db
+	db = MySQLdb.connect(host="ave-la.dynns.com",
+			     user="root",
+			     passwd="ZMalqp10",
+			     db="tablero_dmm2")
+
+def get_event():
+	global id_detalle, id_ot, id_usuario, origen
+
+	codigo =raw_input()
+
+	#DETERMINAMOS SI ES USUARIO U OT
+	if len(codigo)  > 0:
+		if codigo[:4] == "000.":
+			a,b  = codigo.split(".")
+			origen = "USUARIOS"
+		else:
+			id_ot,id_unidad,id_detalle = codigo.split(".")
+			origen = "OT"
+		set_tarea()
+
+def set_tarea():
+	global id_detalle, id_ot, id_usuario, origen, db
+	bandera = false
+
+	if origen == "OT":
+		base()
+
+		cur = db.cursor()
+		cur.execute("SELECT ID_PROCESO, ID_ESTACION, ESTADO, NUM_PROC FROM PROCESO_OT WHERE ID_DETALLE = " + id_detalle + " AND ESTADO = 'EN CURSO' ORDER BY NUM_PROC")
+
+		for row in cur.fetchall():
+			id_proceso = row[0]
+			id_estacion = row[1]
+			estado = row[2]
+			num_proceso = row[3]
+			bandera = true
+		db.close()
+
+		
+
+	elif origen == "USUARIOS":
+		print("ORIGEN")
+
+
+if __name__=="__main__":
+	try:
+		while True:
+			get_event()
+	except KeyboardInterrupt:
+		pass
