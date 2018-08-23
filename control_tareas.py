7ef1483b571c0c2bd52e61606fb009381834e79c
ErickNavarrete <38058419+ErickNavarrete@users.noreply.github.com>
2018-08-23 13:16:41 -0500

    Terminado

--- control_tareas.py
+++ control_tareas.py
@@ -37,7 +37,8 @@ def get_event():
 def set_tarea():
 	global id_detalle, id_ot, id_usuario, origen, db, mydb
 	bandera =  "False"
-
+	estado_t = "True"
+	
 	if origen == "OT":
 		base()
 		cur = db.cursor()
@@ -101,7 +102,108 @@ def set_tarea():
 			cur = db.cursor()
 			sql = "update estacion set estado = 'LIBRE' where id_estacion = %s "
 			val = (id_estacion)
+			
+			try:
+				cur.execute(sql,val)
+				db.commit()
+			except:
+				# Rollback in case there is any error
+				print("ERROR")
+				db.rollback()			
+			db.close()
+			
+			#VERIFICAMOS SI LA ORDEN DE TRABAJO YA FUE COMPLETADA
+			base()
+			cur = db.cursor()
+			sql = "select estado from proceso_ot where id_detalle = %s "
+			val = (id_detalle)
+			cur.execute(sql,val)
+			
+			for row in cur.fetchall():
+				if row[0] <> "TERMINADO":
+					estado_t = "False"					
+			db.close()
+			
+			if estado_t = "True":
+				#ACTUALIZA PROCESO OT
+				base()
+				cur = db.cursor()
+				sql = "update orden_trabajo set momento = 'TERMINADO' where id_ot = %s "
+				val = (id_ot)
+				
+				try:
+					cur.execute(sql,val)
+					db.commit()
+				except:
+					# Rollback in case there is any error
+					print("ERROR")
+					db.rollback()			
+				db.close()
 
+		else:
+			#CONSULTAMOS SI LA ESTACION ESTA LIBRE PARA COMENZAR EL PROCESO
+			base()
+			cur = db.cursor()
+			sql = " select estado from estacion where id_estacion = %s "
+			val = (id_estacion)
+			cur.execute(sql,val)
+			
+			for row in cur.fetchall():
+				if row[0] = "OCUPADO":
+					print("ESTACIÓN EN USO")
+					return			
+			db.close()
+			
+			#INSERTAMOS EN EL HISTORIAL
+			base()
+			cur = db.cursor()
+			fecha = str(time.strftime("20%y/%m/%d %X"))
+			
+			sql = " insert into historial (id_detalle, id_proceso, id_usuario, estado, fecha) values (%s, %s, %s, 'EN CURSO', %s) "
+			val = (id_detalle,id_proceso, id_usuario,fecha)
+			 
+			try:
+				cur.execute(sql,val)
+				db.commit()
+			except:
+				# Rollback in case there is any error
+				print("ERROR")
+				db.rollback()			
+			db.close()
+			
+			#ACTUALIZA PORCESO_OT
+			base()
+			cur = db.cursor()
+			
+			sql = " update proceso_ot set estado = 'EN CURSO' where id_detalle = %s and id_proceso = %s and num_proc = %s "
+			val = (id_detalle,id_proceso,num_proc)
+			 
+			try:
+				cur.execute(sql,val)
+				db.commit()
+			except:
+				# Rollback in case there is any error
+				print("ERROR")
+				db.rollback()			
+			db.close()
+			
+			#ACTUALIZA ESTADO DE LA ESTACION
+			base()
+			cur = db.cursor()
+			
+			sql = " update estacion set estado = 'OCUPADO' where id_estacion = '%s "
+			val = (id_estacion)
+			 
+			try:
+				cur.execute(sql,val)
+				db.commit()
+			except:
+				# Rollback in case there is any error
+				print("ERROR")
+				db.rollback()			
+			db.close()
+			
+		
 	elif origen == "USUARIOS":
 		print("ORIGEN")
 
